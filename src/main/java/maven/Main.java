a7c0228d8ceac5e915fe928649811867f7386c47
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-03-29 19:56:06 +0200

    Initial commit

---
+++ src/main/java/maven/Main.java
@@ -0,0 +1,11 @@
+package maven;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(new Person()));
+    }
+}
